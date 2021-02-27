47ff3553023f6784a21ce9bb3ba558a28ff0afbd
Khoa Le <kdl2483@truman.edu>
2021-02-27 17:39:39 -0600

    Functional stage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # setting the root
-  root "articles#index"
-  get "/articles", to: "articles#index"
+  root "home#index"
+  get "home/about", to: "home#about"
 end
