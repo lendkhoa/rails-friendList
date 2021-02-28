565c43135190e28d5a5293ebb70fe768bdd47e68
Khoa Le <kdl2483@truman.edu>
2021-02-28 15:47:07 -0600

    v 1.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :friends
-  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # setting the root
   root "home#index"
   get "home/about", to: "home#about"
+  devise_for :users
+  resources :friends
 end
